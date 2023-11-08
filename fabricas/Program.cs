a3f42af14df17348ff297dcbfcce1c2845a988db
Емельянов Всеволод Салаватович <212209@mck-ktits.ru>
2023-11-08 13:10:45 +0300

    first commit

---
+++ fabricas/Program.cs
@@ -0,0 +1,53 @@
+﻿public class Game
+{
+    public void PlayGame()
+    {
+        //запускаем игру
+    }
+
+    public void EscapeGame()
+    {
+        //выходимс из игры
+    }
+
+    public void PassGame()
+    {
+        //проходим игру
+    }
+}
+
+public class Arcade : Game { }
+public class Adventure : Game { }
+public class Alive : Game { }
+public class Shooter : Game { }
+public class Novell : Game { }
+
+public static class GamesStorage
+{
+    public static Game BuyGame(string type)
+    {
+        Game game = null;
+
+        switch (type)
+        {
+            case "ARCADE":
+                game = new Arcade();
+                break;
+            case "ADVENTURE":
+                game = new Adventure();
+                break;
+            case "ALIVE":
+                game = new Alive();
+                break;
+            case "SHOOTER":
+                game = new Shooter();
+                break;
+            case "NOVELL":
+                game = new Novell();
+                break;
+        }
+
+        Console.WriteLine($"Спасибо за покупку {game.GetType()}! Пополните копилку Габена ещё!");
+        return game;
+    }
+}
